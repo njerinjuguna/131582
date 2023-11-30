4047a30fa21013a1bf251b436ab21e364e7987d0
njerinjuguna <joyce.njuguna@strathmore.edu>
2023-11-30 19:50:18 +0300

    Update scraper

--- scraper/app/Http/Controllers/ScraperController.php
+++ scraper/app/Http/Controllers/ScraperController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Place;
 use Illuminate\Http\Request;
 use Goutte\Client;
 
@@ -11,11 +12,56 @@ class ScraperController extends Controller
     public function index() {
 
         $client = new Client();
-        $website = $client->request('GET', 'https://www.airbnb.com/');
+        $website = $client->request('GET', 'https://www.kayak.com/Nairobi-Hotels.26243.hotel.ksp');
 
-        // dd($client);
-        
-        dd($website->html());
+        $placeDivs = $website->filter('.c44F-item');
+        $placeNames = [];
+        $placePrices = [];
+        $placeRatings = [];
+        $placeImages = [];
+        $placeURLs = [];
+
+        // Filter through the divs and save the scraped data to our places database
+        $placeDivs->each(function ($placeDiv) use(&$placeNames, &$placePrices, &$placeRatings, &$placeImages, &$placeURLs) {
+            $placeName = $placeDiv->filter('.soom-name')->text();
+            $placePrice = $placeDiv->filter('.soom-price')->text();
+            $placeRating = $placeDiv->filter('.soom-rating-wrapper')->text();
+            $placeImage = $placeDiv->filter('img.EWHU')->attr('src');
+            $placeURL = $placeDiv->filter('.soom-photo-wrapper')->attr('href');
+            
+            $price = preg_replace("/[^0-9]/", "", $placePrice);
+            $rating = preg_replace("/[^0-9.]/", "", $placeRating);
+            $image = strstr($placeImage, '?', true) ?: $placeImage;
+            $url = "https://www.kayak.com" . $placeURL;
+
+            if ($image == null) return;
+
+            $placeNames[] = $placeName;
+            $placePrices[] = $price;
+            $placeRatings[] = $rating;
+            $placeImages[] = $image;
+            $placeURLs[] = $url;
+
+            $place = new Place();
+            $place->name = $placeName;
+            $place->price = $price;
+            $place->image_path = $image;
+            $place->booking_url = $url;
+
+            $place->rating = $rating;
+            $place->description = "Lorem Ipsum";
+            $place->location_url = "https://example.com";
+            $place->type = "Hotel";
+            $place->bedrooms = 5;
+            $place->bathrooms = 5;
+            $place->max_guests = 2;
+
+            $place->save();
+
+        });
+                
+
+        // dd($placeImages);
 
     }
 }
